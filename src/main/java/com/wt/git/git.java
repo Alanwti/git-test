edba3b1e1ed8e96a867b39cd2d724d8f5ead2a40
Alan <Alan@wt.com>
2022-08-01 22:05:33 +0800

    first commit

---
+++ src/main/java/com/wt/git/git.java
@@ -0,0 +1,10 @@
+package com.wt.git;
+
+import com.sun.scenario.effect.impl.sw.java.JSWBlend_SRC_OUTPeer;
+import org.w3c.dom.ls.LSOutput;
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
